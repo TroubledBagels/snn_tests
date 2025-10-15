eb7297b5084fa2f6287a9a6b4feb009f5813f0c5
Ben Hatton <hatton.benjamin@gmail.com>
2025-10-15 18:48:13 +0100

    visualisation

--- models/SimpleConv.py
+++ models/SimpleConv.py
@@ -3,7 +3,6 @@ import torch.nn as nn
 import snntorch as snn
 import snntorch.surrogate
 
-'''
 class SimpleConvModel(nn.Module):
     def __init__(self, in_c, out_c):
         super(SimpleConvModel, self).__init__()
@@ -53,54 +52,53 @@ class SimpleConvModel(nn.Module):
         out = torch.stack(spk_rec).mean(dim=0)
 
         return out, (mem1, mem2)
-'''
 
-class SimpleConvModel(nn.Module):
-    def __init__(self, in_c, out_c):
-        super(SimpleConvModel, self).__init__()
-        self.spike_grad = snn.surrogate.atan()
-        self.conv1 = nn.Conv2d(in_c, 16, kernel_size=5, stride=2, padding=1)
-        self.lif1 = snn.Leaky(beta=0.9, spike_grad=self.spike_grad)
-        self.conv2 = nn.Conv2d(16, 32, kernel_size=5, stride=2, padding=1)
-        self.lif2 = snn.Leaky(beta=0.9, spike_grad=self.spike_grad)
-        self.gap = nn.AdaptiveAvgPool2d((1, 1))
-        self.flatten = nn.Flatten()
-        self.fc1 = nn.Linear(32, 256)
-        self.dropout1 = nn.Dropout(p=0.35)
-        self.lif3 = snn.Leaky(beta=0.9, spike_grad=self.spike_grad)
-        self.fc2 = nn.Linear(256, out_c)
-        self.dropout2 = nn.Dropout(p=0.35)
-        self.lif4 = snn.Leaky(beta=0.9, spike_grad=self.spike_grad)
-
-    def forward(self, x):
-        B, T, C, H, W = x.shape
-        mem1 = self.lif1.init_leaky()
-        mem2 = self.lif2.init_leaky()
-        mem3 = self.lif3.init_leaky()
-        mem4 = self.lif4.init_leaky()
-        spk_rec = []
-
-        for t in range(T):
-            xt = x[:, t, :, :, :]  # Shape: (B, C, H, W)
-            xt = self.conv1(xt)
-            xt, mem1 = self.lif1(xt, mem1)
-            xt = self.conv2(xt)
-            xt, mem2 = self.lif2(xt, mem2)
-            xt = self.gap(xt)
-            xt = self.flatten(xt)
-            xt = self.fc1(xt)
-            xt = self.dropout1(xt)
-            xt, mem3 = self.lif3(xt, mem3)
-            xt = self.fc2(xt)
-            xt = self.dropout2(xt)
-            xt, mem4 = self.lif4(xt, mem4)
-            # spk_rec.append(xt)
-            spk_rec.append(mem4)
-
-        # out = torch.stack(spk_rec)
-        out = torch.stack(spk_rec).mean(dim=0)
-
-        return out, (mem1, mem2)
+# class SimpleConvModel(nn.Module):
+#     def __init__(self, in_c, out_c):
+#         super(SimpleConvModel, self).__init__()
+#         self.spike_grad = snn.surrogate.atan()
+#         self.conv1 = nn.Conv2d(in_c, 16, kernel_size=5, stride=2, padding=1)
+#         self.lif1 = snn.Leaky(beta=0.9, spike_grad=self.spike_grad)
+#         self.conv2 = nn.Conv2d(16, 32, kernel_size=5, stride=2, padding=1)
+#         self.lif2 = snn.Leaky(beta=0.9, spike_grad=self.spike_grad)
+#         self.gap = nn.AdaptiveAvgPool2d((1, 1))
+#         self.flatten = nn.Flatten()
+#         self.fc1 = nn.Linear(32, 256)
+#         self.dropout1 = nn.Dropout(p=0.35)
+#         self.lif3 = snn.Leaky(beta=0.9, spike_grad=self.spike_grad)
+#         self.fc2 = nn.Linear(256, out_c)
+#         self.dropout2 = nn.Dropout(p=0.35)
+#         self.lif4 = snn.Leaky(beta=0.9, spike_grad=self.spike_grad)
+#
+#     def forward(self, x):
+#         B, T, C, H, W = x.shape
+#         mem1 = self.lif1.init_leaky()
+#         mem2 = self.lif2.init_leaky()
+#         mem3 = self.lif3.init_leaky()
+#         mem4 = self.lif4.init_leaky()
+#         spk_rec = []
+#
+#         for t in range(T):
+#             xt = x[:, t, :, :, :]  # Shape: (B, C, H, W)
+#             xt = self.conv1(xt)
+#             xt, mem1 = self.lif1(xt, mem1)
+#             xt = self.conv2(xt)
+#             xt, mem2 = self.lif2(xt, mem2)
+#             xt = self.gap(xt)
+#             xt = self.flatten(xt)
+#             xt = self.fc1(xt)
+#             xt = self.dropout1(xt)
+#             xt, mem3 = self.lif3(xt, mem3)
+#             xt = self.fc2(xt)
+#             xt = self.dropout2(xt)
+#             xt, mem4 = self.lif4(xt, mem4)
+#             # spk_rec.append(xt)
+#             spk_rec.append(mem4)
+#
+#         # out = torch.stack(spk_rec)
+#         out = torch.stack(spk_rec).mean(dim=0)
+#
+#         return out, (mem1, mem2)
 
 if __name__ == "__main__":
     device = torch.device("cuda" if torch.cuda.is_available() else "cpu")
