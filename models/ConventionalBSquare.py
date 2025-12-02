16e8c49c91acee1c2fee02cd276ff81fbd92ab0b
Ben Hatton <hatton.benjamin@gmail.com>
2025-12-02 11:14:56 +0000

    quick fix

--- models/ConventionalBSquare.py
+++ models/ConventionalBSquare.py
@@ -106,7 +106,7 @@ class TinyCNN(nn.Module):
         # self.bn4 = nn.BatchNorm2d(64)
         self.gap = nn.AdaptiveAvgPool2d(4)
         self.fc1 = nn.Linear(64 * 4 * 4, 512)
-        self.fc2 = nn.Linear(512, 10)
+        self.fc2 = nn.Linear(512, 2)
 
     def forward(self, x):
         x = self.conv1(x)
