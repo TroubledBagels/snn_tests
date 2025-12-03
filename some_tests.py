5392e1f7c8e355d372adfb9fd04fb6aabc9c5f2a
Ben Hatton <hatton.benjamin@gmail.com>
2025-12-03 14:38:02 +0000

    quick fix

--- some_tests.py
+++ some_tests.py
@@ -1,15 +1,19 @@
-import torch
-import tonic
-import pathlib
+import pandas as pd
 
-home = pathlib.Path.home()
-data_path = home / "data" / "nmnist"
+result_df = pd.DataFrame(columns=["Class_Pair", "TinyCNN", "SmallCNN", "SeparableSmallCNN", "MediumCNN", "SeparableMediumCNN"])
 
-ds = tonic.datasets.NMNIST(train=True, save_to=data_path)
-to_frame = tonic.transforms.ToFrame(
-    n_time_bins=300,
-    sensor_size=tonic.datasets.NMNIST.sensor_size
-)
-data, label = ds[0]
-frame = to_frame(data)
-print(f"Data shape after ToFrame transform: {frame.shape}")
+data_dict = {
+    "Class_Pair": "1 v 2",
+    "TinyCNN": 0.9730,
+    "SmallCNN": 0.9850,
+    "SeparableSmallCNN": 0.9800,
+    "MediumCNN": 0.9900,
+    "SeparableMediumCNN": 0.9880
+}
+
+print(result_df)
+
+result_df.loc[len(result_df)] = data_dict
+print(result_df)
+result_df.loc[len(result_df)] = data_dict
+print(result_df)
