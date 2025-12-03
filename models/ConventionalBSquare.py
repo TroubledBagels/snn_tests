9773927c1f5bfbe4757241e549251eb735275308
Ben Hatton <hatton.benjamin@gmail.com>
2025-12-03 12:36:02 +0000

    quick fix

--- models/ConventionalBSquare.py
+++ models/ConventionalBSquare.py
@@ -245,6 +245,60 @@ class SeparableSmallCNN(nn.Module):
         # x = self.fc2(x)
         return x, None
 
+class MediumCNN(nn.Module):
+    def __init__(self, c_1, c_2, hid, inp, out, num_layers=2):
+        self.c_1 = c_1
+        self.c_2 = c_2
+        self.conv1 = nn.Conv2d(3, 32, kernel_size=3, stride=1, padding=1)
+        self.bn1 = nn.BatchNorm2d(32)
+        self.do = nn.Dropout(0.2)
+        self.conv2 = nn.Conv2d(32, 32, kernel_size=3, stride=1, padding=1)
+        self.bn2 = nn.BatchNorm2d(32)
+        self.pool = nn.MaxPool2d(kernel_size=2, stride=2, padding=0)
+        self.conv3 = nn.Conv2d(32, 64, kernel_size=3, stride=1, padding=1)
+        self.bn3 = nn.BatchNorm2d(64)
+        self.conv4 = nn.Conv2d(64, 64, kernel_size=3, stride=1, padding=1)
+        self.bn4 = nn.BatchNorm2d(64)
+        self.conv5 = nn.Conv2d(64, 64, kernel_size=3, stride=1, padding=1)
+        self.bn5 = nn.BatchNorm2d(64)
+        self.conv6 = nn.Conv2d(64, 64, kernel_size=3, stride=1, padding=1)
+        self.bn6 = nn.BatchNorm2d(64)
+        self.gap = nn.AdaptiveAvgPool2d(4)
+        # self.gap = nn.MaxPool2d(2, 2)
+        self.fc1 = nn.Linear(64 * 4 * 4, 256)
+        self.fc2 = nn.Linear(256, 2)
+
+    def forward(self, x):
+        x = self.conv1(x)
+        x = self.bn1(x)
+        x = torch.relu(x)
+        x = self.do(x)
+        x = self.conv2(x)
+        x = self.bn2(x)
+        x = torch.relu(x)
+        x = self.pool(x)
+        x = self.conv3(x)
+        x = self.bn3(x)
+        x = torch.relu(x)
+        x = self.do(x)
+        x = self.conv4(x)
+        x = self.bn4(x)
+        x = torch.relu(x)
+        x = self.pool(x)
+        x = self.conv5(x)
+        x = self.bn5(x)
+        x = torch.relu(x)
+        x = self.do(x)
+        x = self.conv6(x)
+        x = self.bn6(x)
+        x = torch.relu(x)
+        x = self.gap(x)
+        x = nn.Flatten()(x)
+        x = self.fc1(x)
+        x = torch.relu(x)
+        x = self.fc2(x)
+        return x, None
+
 class BSquareModel(nn.Module):
     def __init__(self, num_classes: int, input_size=30, hidden_size=32, num_layers=2, binary_voting=False, bclass=BClassifier, net_out=False):
         super(BSquareModel, self).__init__()
