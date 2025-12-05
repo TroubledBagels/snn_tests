bc3beef29da504274e1201fe3978345320b55daa
Ben Hatton <hatton.benjamin@gmail.com>
2025-12-05 16:18:20 +0000

    quick fix

--- cifar10_experiments.py
+++ cifar10_experiments.py
@@ -21,9 +21,9 @@ class BClassModel(nn.Module):
         self.bn3 = nn.BatchNorm2d(64)
         self.conv4 = nn.Conv2d(64, 64, kernel_size=3, stride=1, padding=1)
         self.bn4 = nn.BatchNorm2d(64)
-        self.gap = nn.AdaptiveAvgPool2d(1)
+        self.gap = nn.AdaptiveAvgPool2d(2)
         # self.gap = nn.MaxPool2d(2, 2)
-        self.fc1 = nn.Linear(64 * 1 * 1, 2)
+        self.fc1 = nn.Linear(64 * 2 * 2, 2)
         # self.fc2 = nn.Linear(256, 10)
 
     def forward(self, x):
