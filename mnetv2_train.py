ea692324e67a2ddd79440f0199c8d1621de44c93
Ben Hatton <hatton.benjamin@gmail.com>
2025-12-10 17:38:30 +0000

    quick fix

--- mnetv2_train.py
+++ mnetv2_train.py
@@ -37,7 +37,7 @@ if __name__ == "__main__":
     tr_dl = DataLoader(tr_ds, batch_size=64, shuffle=True)
     te_dl = DataLoader(te_ds, batch_size=1, shuffle=False)
 
-    model = WideResNet(num_classes=10)
+    model = MobileNetV3(num_classes=10)
 
     criterion = nn.CrossEntropyLoss()
     optimiser = torch.optim.Adam(model.parameters(), lr=0.001)
