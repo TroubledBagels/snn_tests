86340ab30b30fef12baa6083a7410087bfd1cc6e
Ben Hatton <hatton.benjamin@gmail.com>
2025-12-20 11:25:19 +0000

    quick fix

--- cifar10_square.py
+++ cifar10_square.py
@@ -44,8 +44,8 @@ if __name__ == '__main__':
     print(f"Parameters: m: {model_dir}, t: {threshold}, i: {inference_only}, b: {binary_voting}, s: {similarity_weighting}, ns: {no_softmax}, g: {graph_readout}")
 
     augmentation_transform = torchvision.transforms.Compose([
-        torchvision.transforms.RandomHorizontalFlip(),
-        torchvision.transforms.RandomCrop(32, padding=4),
+        # torchvision.transforms.RandomHorizontalFlip(),
+        # torchvision.transforms.RandomCrop(32, padding=4),
         torchvision.transforms.ToTensor(),
         torchvision.transforms.Normalize(mean=[0.4914, 0.4822, 0.4465], std=[0.2023, 0.1994, 0.2010])
     ])
@@ -57,8 +57,8 @@ if __name__ == '__main__':
 
     home_dir = pathlib.Path.home()
     save_dir = home_dir / "data" / "cifar10"
+    tr_ds = torchvision.datasets.CIFAR10(root=save_dir, train=True, transform=test_transform, download=True)
     te_ds = torchvision.datasets.CIFAR10(root=save_dir, train=False, transform=test_transform, download=True)
-    tr_ds = torchvision.datasets.CIFAR10(root=save_dir, train=True, transform=augmentation_transform, download=True)
     print(f"Train size: {len(tr_ds)}, Test size: {len(te_ds)}")
     print(tr_ds[0][0].shape)
 
